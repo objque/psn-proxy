302d6c149131861cd5e73c057e28b534c94e1254
Mikhail Kalinin <m.kalinin@selectel.ru>
2018-07-19 11:47:44 +0300

    Update proxy answer

--- psn/valkyrie.py
+++ psn/valkyrie.py
@@ -4,14 +4,35 @@ import requests
 class ValkyrieAPIClient(object):
     def _fix_resolve_answer(self, json):
         included = json["included"][0]
+        price = included["attributes"]["skus"][0]["prices"]["non-plus-user"]
+        plus_price = included["attributes"]["skus"][0]["prices"]["plus-user"]
         return {
             "id": included["id"],
-            "type": included["type"],
             "name": included["attributes"]["name"],
-            "released": included["attributes"]["release-date"],
             "poster": included["attributes"]["thumbnail-url-base"],
-            "score": included["attributes"]["star-rating"],
-            "prices": included["attributes"]["skus"][0]["prices"],
+            "type": included["type"],
+            "released": included["attributes"]["release-date"],
+            "price": price["strikethrough-price"]["value"] / 100,
+            "rate": {
+                "total": included["attributes"]["star-rating"]["total"],
+                "value": included["attributes"]["star-rating"]["score"],
+            },
+            "discounts": [
+                {
+                    "is_plus": price["is-plus"],
+                    "value": price["actual-price"]["value"] / 100,
+                    "percentage": price["discount-percentage"],
+                    "since": price["availability"]["start-date"],
+                    "till": price["availability"]["end-date"],
+                },
+                {
+                    "is_plus": plus_price["is-plus"],
+                    "value": plus_price["actual-price"]["value"] / 100,
+                    "percentage": plus_price["discount-percentage"],
+                    "since": plus_price["availability"]["start-date"],
+                    "till": plus_price["availability"]["end-date"],
+                },
+            ]
         }
 
     def resolve(self, id):
